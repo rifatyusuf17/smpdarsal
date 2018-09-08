af515f7ce0a4d256570d22342093fa66492b85e1
Rifat yusuf <rifatyusuf17@gmail.com>
2018-09-08 21:16:25 +0700

    satu

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SMP Terpadu Darussalam</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <style>
+  h1 {
+  	color: green;
+  	font-family: arial;
+  }
+
+
+  </style>
+</head>
+<body>
+  
+<div class="container col-lg-12" style="text-align: center;">
+  	<br>
+  	<img src="assets/img/ds.jpg" style="width:10%" >
+  	<h1>SMP TERPADU DARUSSALAM</h1>
+  <div class="container">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">HOME</a></li>
+      <li><a href="#">PROFILE</a></li>
+      <li><a href="#">INFORMASI</a></li>
+      <li><a href="#">GALLERY</a></li>
+      <li><a href="#">PMB</a></li>
+    </ul>
+  </div>.
+</div>
+
+  <div class="col-lg-12" style="text-align: center;">
+test
+  </div>
+
+</body>
+</html>
